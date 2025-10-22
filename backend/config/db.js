22122098a9410e4456f74e9f676d5792d18bcf50
Haripriya Addepalli <your_email@example.com>
2025-10-22 14:34:36 +0530

    Initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      process.env.MONGO_URL
-    )
-    .then(() =>console.log("DB Connected"));
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1); // exit process if DB connection fails
+  }
 };
